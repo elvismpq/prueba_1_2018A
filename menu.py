6b7da873e140934bb7787a3b388f2ed8fbdb66d6
LAPTOP-8QGKE15J\antho <anthony.tacuri@epn.edu.ec>
2018-05-02 17:02:47 -0500

    Funcion menu

---
+++ menu.py
@@ -0,0 +1,13 @@
+def menu():
+    print('\t\t\tMenu')
+    print('Elija una figura para calcular el volumen:')
+    print('1. Esfera')
+    print('2. Piramide Triangular')
+    print('3. Cubo')
+    print('4. Salir')
+
+
+
+
+
+
